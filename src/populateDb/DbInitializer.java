0ebb5cce30cfab6622544a7d1fd007fd96034580
Niels Wauters <niels.wauters.x7374@student.hogent.be>
2015-03-28 12:58:59 +0100

    Toevoeging van Landen en werelddelen

--- src/populateDb/DbInitializer.java
+++ src/populateDb/DbInitializer.java
@@ -5,7 +5,12 @@
  */
 package populateDb;
 
+import domein.Land;
 import domein.Werelddeel;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import repository.GenericDaoJpa;
 import repository.WerelddeelDaoJpa;
 
 /**
@@ -13,15 +18,42 @@ import repository.WerelddeelDaoJpa;
  * @author Asus Wauters Niels
  */
 public class DbInitializer {
-     public void run() {
-       WerelddeelDaoJpa werelddeeldao = new WerelddeelDaoJpa();
+
+    public void run() {
+
+        //        WerelddeelDaoJpa werelddeeldao = new WerelddeelDaoJpa();
+        //INITIALIZEREN VAN DE DAO'S
+        GenericDaoJpa werelddeeldao = new GenericDaoJpa(Werelddeel.class);
+        GenericDaoJpa landdao = new GenericDaoJpa(Land.class);
+
+        //WERELDDELEN
+        Werelddeel afrika = new Werelddeel("Afrika");
+        Werelddeel europa = new Werelddeel("Europa");
+
+        //LANDEN
+        Land belgie = new Land("Belgie");
+
+        //COLLECTIONS
+        Collection<Werelddeel> werelddelen = new ArrayList<>(Arrays.asList(afrika,europa));
+        Collection<Land> landenEuropa = new ArrayList<>(Arrays.asList(belgie));
+
+        //TOEVOEGING VAN COLLECTIONS
+        europa.setLanden(landenEuropa);
+
+        //START INITIALIZE
         werelddeeldao.startTransaction();
-        werelddeeldao.insert(new Werelddeel("Afrika"));
-          werelddeeldao.insert(new Werelddeel("Europa"));
-              werelddeeldao.insert(new Werelddeel("Azie"));
-//        GenericDaoJpa winkeldao = new GenericDaoJpa(Winkel.class);
-//        winkeldao.insert(new Winkel("Putteke"));
-//        winkeldao.insert(new Winkel("Op den hoek"));
+
+
+        for(Werelddeel werelddeel:werelddelen){
+            werelddeeldao.insert(werelddeel);
+        }
+
+        for (Land land : landenEuropa) {
+            landdao.insert(land);
+        }
+
+//      
         werelddeeldao.commitTransaction();
+
     }
 }
